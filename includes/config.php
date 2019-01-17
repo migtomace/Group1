62b5ce18c282ea8be0bf89a15044524801d1f7e9
Miguel Acevedo <migtomace@gmail.com>
2019-01-16 18:50:10 -0800

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Miguel.Acevedo
+ * Date: 1/15/2019
+ * Time: 6:29 PM
+ */
+
+//connect to database
+
+//
---
+++ includes/dbh.php
@@ -0,0 +1,7 @@
+<?php
+$dbServername = "mysql.url.com";
+$dbUsername = "migace1";
+$dbPassword = "password";
+$dbName = "dbname";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
